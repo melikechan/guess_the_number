70d2561c7cefdb51d0700eccdb67a4c193bcf4f5
Melike VURUCU <melikev63@gmail.com>
2021-01-18 15:43:06 +0300

    initial game layout

--- js/game.js
+++ js/game.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const guessField = document.getElementById('guessInput');
+const button = document.getElementById('guessButton');
+
+button.addEventListener('click', makeGuess);
+
+const makeGuess = () => {
+  
+};
